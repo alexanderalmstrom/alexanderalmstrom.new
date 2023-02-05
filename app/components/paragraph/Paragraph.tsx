df0badf3b826d75d9aaf80d99800996f616b07f6
Alexander Almström <info@alexanderalmstrom.com>
2023-02-06 00:54:45 +0100

    Paragraph variants

--- app/components/paragraph/Paragraph.tsx
+++ app/components/paragraph/Paragraph.tsx
@@ -1,21 +1,29 @@
-import { suisseMono } from "@/app/fonts";
-import { cx } from "class-variance-authority";
+import { suisseIntl, suisseMono } from "@/app/fonts";
+import { cva, cx, type VariantProps } from "class-variance-authority";
 
-export interface ParagraphProps {
+export const paragraph = cva(["max-w-3xl leading-relaxed"], {
+  variants: {
+    style: {
+      sansSerif: [suisseIntl.className],
+      mono: [suisseMono.className],
+    },
+  },
+  defaultVariants: {
+    style: "mono",
+  },
+});
+
+export type ParagraphVariantProps = VariantProps<typeof paragraph>;
+
+export interface ParagraphProps extends ParagraphVariantProps {
   className?: string;
   children: React.ReactNode;
 }
 
-export default function Paragraph({ className, children }: ParagraphProps) {
-  return (
-    <p
-      className={cx(
-        className,
-        "max-w-3xl leading-relaxed",
-        suisseMono.className
-      )}
-    >
-      {children}
-    </p>
-  );
+export default function Paragraph({
+  className,
+  children,
+  style,
+}: ParagraphProps) {
+  return <p className={cx(className, paragraph({ style }))}>{children}</p>;
 }
